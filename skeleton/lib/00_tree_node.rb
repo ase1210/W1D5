ee6a6d74e3889557248fc5944fd1d7d7671f4dd0
Drew <andrew.engelstein@gmail.com>
2019-01-11 15:36:19 -0800

    Add #initialize, #parent=, #add_child, #remove_child to PolyTreeNode class

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,38 @@
+require 'byebug'
+
 class PolyTreeNode
+
+    attr_reader :children, :parent, :value
+
+    def initialize(value)
+        @parent = nil
+        @children = []
+        @value = value
+    end
+
+    def parent=(node)
+        if @parent == nil 
+            @parent = node 
+            node.children << self unless node.children.include?(self)
+        elsif node == nil
+            old_parent = self.parent 
+            old_parent.children.delete(self)
+            @parent = node
+        else 
+            old_parent = self.parent 
+            old_parent.children.delete(self)
+            @parent = node
+            node.children << self unless node.children.include?(self)
+        end
+    end
+
+    def add_child(child_node)
+        child_node.parent = self
+    end
+
+    def remove_child(child_node)
+        child_node.parent = nil
+        raise unless children.include?(child_node)
+    end
 
 end
